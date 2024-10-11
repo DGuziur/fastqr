1bf4e8efaaf8d2cd13d767d4f37244bf17530a83
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-10-11 22:49:14 +0200

    add textarea to change qr string

--- src/app/features/qr-generator/qr-generator.component.ts
+++ src/app/features/qr-generator/qr-generator.component.ts
@@ -1,18 +1,40 @@
-import { Component, ElementRef, signal, viewChild } from '@angular/core';
+import {
+  Component,
+  effect,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzColorPickerComponent } from 'ng-zorro-antd/color-picker';
+import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+type ErrorCodeLevel = 'L' | 'M' | 'Q' | 'H';
 
 @Component({
   selector: 'qr-generator',
   standalone: true,
-  imports: [NzButtonModule, NzQRCodeModule],
+  imports: [
+    NzButtonModule,
+    NzSegmentedModule,
+    NzQRCodeModule,
+    NzColorPickerComponent,
+    NzInputModule,
+  ],
   templateUrl: './qr-generator.component.html',
   styleUrl: './qr-generator.component.scss',
 })
 export class QrGeneratorComponent {
   private qr = viewChild.required<ElementRef>('qr');
   private download = viewChild.required<ElementRef>('download');
+
+  errorCodeLevels: ErrorCodeLevel[] = ['L', 'M', 'Q', 'H'];
+
   qrValue = signal(window.location.href);
+  qrColor = signal('rgb(0, 0, 0)');
+  qrLevel = signal<ErrorCodeLevel>('L');
 
   downloadQR() {
     const canvas = this.qr().nativeElement.querySelector('canvas');
