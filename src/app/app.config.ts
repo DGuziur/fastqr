fc47ab6d25bfdabc62ae9142b54256d89e5cc8c3
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-10-11 17:43:37 +0200

    delete unused files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,7 @@ import {
   provideZoneChangeDetection,
   importProvidersFrom,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -17,7 +15,6 @@ registerLocaleData(en);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideNzI18n(en_US),
     importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
