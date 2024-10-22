2003d165dfc8ed339382c07965b5bf8b67772ea8
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-10-22 14:01:46 +0200

    fix download and copy

--- src/app/features/qr-generator/qr-generator.component.ts
+++ src/app/features/qr-generator/qr-generator.component.ts
@@ -2,49 +2,38 @@ import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  effect,
   ElementRef,
   signal,
   viewChild,
 } from '@angular/core';
-import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzColor, NzColorPickerComponent } from 'ng-zorro-antd/color-picker';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { SegmentedComponent } from '../../components/segmented/segmented.component';
-import { QrcodeComponent } from '../../components/qrcode/qrcode.component';
+import { MatIconModule } from '@angular/material/icon';
+
+import QrCode from 'qrcode';
 
 export type ErrorCodeLevel = 'L' | 'M' | 'Q' | 'H';
-type ColorEvent = { color: NzColor; format: string };
 
 @Component({
   selector: 'qr-generator',
   standalone: true,
-  imports: [
-    NzButtonModule,
-    NzQRCodeModule,
-    NzColorPickerComponent,
-    NzIconModule,
-    NzInputModule,
-    NzToolTipModule,
-    SegmentedComponent,
-    QrcodeComponent,
-  ],
+  imports: [MatButtonModule, MatIconModule, SegmentedComponent],
   templateUrl: './qr-generator.component.html',
   styleUrl: './qr-generator.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QrGeneratorComponent implements AfterViewInit {
-  private qr = viewChild.required<ElementRef<HTMLDivElement>>('qr');
   private download =
     viewChild.required<ElementRef<HTMLAnchorElement>>('download');
   private qrTextarea =
     viewChild.required<ElementRef<HTMLTextAreaElement>>('qrTextarea');
+  private canvas = viewChild.required<ElementRef<HTMLCanvasElement>>('canvas');
 
   errorCodeLevels: ErrorCodeLevel[] = ['L', 'M', 'Q', 'H'];
 
-  qrValue = signal(window.location.href);
+  qrValue = signal(window.location.host);
+  qrBackground = signal('#fff');
   qrColor = signal('#000');
   qrLevel = signal<ErrorCodeLevel>('L');
   qrIcon = signal('');
@@ -56,8 +45,20 @@ export class QrGeneratorComponent implements AfterViewInit {
     this.qrTextarea().nativeElement.select();
   }
 
+  paint = effect(() => {
+    this.paintQR();
+  });
+
+  paintQR(): void {
+    QrCode.toCanvas(this.canvas().nativeElement, this.qrValue(), {
+      width: 200,
+      color: { light: this.qrBackground(), dark: this.qrColor() },
+      errorCorrectionLevel: this.qrLevel(),
+    });
+  }
+
   downloadQR() {
-    const canvas = this.qr().nativeElement.querySelector('canvas');
+    const canvas = this.canvas().nativeElement;
     if (!canvas) throw console.error('Error while getting qr image');
     this.download().nativeElement.href = canvas.toDataURL('image/png');
     this.download().nativeElement.download = `${this.qrValue()}.png`;
@@ -65,7 +66,7 @@ export class QrGeneratorComponent implements AfterViewInit {
   }
 
   copyToClipboard() {
-    const canvas = this.qr().nativeElement.querySelector('canvas');
+    const canvas = this.canvas().nativeElement;
     if (!canvas) throw console.error('Error while getting qr image');
     canvas.toBlob((blob: Blob | null) => {
       if (!blob) throw console.error('Error while converting qr image to blob');
@@ -113,8 +114,14 @@ export class QrGeneratorComponent implements AfterViewInit {
   protected handleColorChange(e: Event) {
     const target = e.target as HTMLInputElement;
     if (!target.value) throw console.error('No color selected');
-    console.log(target.value);
     this.qrColor.set(target.value);
+    this.isCurrentlyCopied.set(false);
+  }
+
+  protected handleBackgroundChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    if (!target.value) throw console.error('No color selected');
+    this.qrBackground.set(target.value);
     this.isCurrentlyCopied.set(false);
   }
 
