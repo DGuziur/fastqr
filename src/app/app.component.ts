d4a5fd922a09ff130038649c79d2530157cbcb16
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-10-23 21:33:34 +0200

    custom scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { QrGeneratorComponent } from './features/qr-generator/qr-generator.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatTabsModule, QrGeneratorComponent],
+  imports: [MatIconModule, MatTabsModule, QrGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
