4643e50ab8f1e82f75105b099741586dbd720034
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-10-25 20:07:42 +0200

    draggable history items

--- src/app/features/history/history.component.ts
+++ src/app/features/history/history.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Component, viewChild } from '@angular/core';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
 import { DatePipe } from '@angular/common';
+import {
+  CdkDragDrop,
+  CdkDropList,
+  CdkDrag,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
 
 type HistoryItem = {
   createdAt: Date | string;
@@ -15,7 +26,7 @@ type HistoryItem = {
 @Component({
   selector: 'history',
   standalone: true,
-  imports: [DatePipe, MatTableModule],
+  imports: [CdkDropList, CdkDrag, DatePipe, MatIconModule, MatTableModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
 })
@@ -50,5 +61,15 @@ export class HistoryComponent {
     },
   ];
 
-  dataSource = new MatTableDataSource<HistoryItem>(this.mockHistory);
+  dataSource = this.mockHistory;
+  table = viewChild.required<MatTable<HistoryItem>>('table');
+
+  drop(event: CdkDragDrop<string>) {
+    const previousIndex = this.dataSource.findIndex(
+      (d) => d === event.item.data
+    );
+
+    moveItemInArray(this.dataSource, previousIndex, event.currentIndex);
+    this.table().renderRows();
+  }
 }
