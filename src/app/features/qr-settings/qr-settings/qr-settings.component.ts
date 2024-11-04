e4860861a378c5ec5f1c14e61f75d4804301d9aa
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-11-04 16:50:10 +0100

    g c qr-settings

---
+++ src/app/features/qr-settings/qr-settings/qr-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './qr-settings.component.html',
+  styleUrl: './qr-settings.component.scss'
+})
+export class QrSettingsComponent {
+
+}
