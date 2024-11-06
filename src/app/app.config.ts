a2a712c762e1efae2ad9505c4a0d264457d71ecd
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-11-06 14:11:27 +0100

    disable ripple

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,26 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  MAT_RIPPLE_GLOBAL_OPTIONS,
+  RippleGlobalOptions,
+} from '@angular/material/core';
 
 registerLocaleData(en);
+
+const globalRippleConfig: RippleGlobalOptions = {
+  disabled: true,
+  animation: {
+    enterDuration: 0,
+    exitDuration: 0,
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
+    { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig },
   ],
 };
