5ac3d57fefe91c1a00853942d9a6c0ccfd82c9f0
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-11-13 14:25:56 +0100

    move padding to style tab

--- src/app/services/userSettings.service.ts
+++ src/app/services/userSettings.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 
+export const DEFAULT_SETTINGS = {
+  qrLocked: false,
+};
+
+export type UserSettings = typeof DEFAULT_SETTINGS;
+
 @Injectable({
   providedIn: 'root',
 })
-export class UserSettingsService {}
+export class UserSettingsService {
+  qrLocked = false;
+}
