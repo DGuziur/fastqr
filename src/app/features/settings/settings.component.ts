6798901bf5fb7f22a25bc9d6b5ea7e1f6d62424a
DGuziur <dondxd@o2.pl>
2025-01-19 23:55:14 +0100

    feat: user preferences settings

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDivider } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'settings',
-  imports: [MatCardModule, MatSlideToggleModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatDivider,
+    MatInputModule,
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
